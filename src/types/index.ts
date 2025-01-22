fb3f63cbdb071cf6baed411f28b6f78323d8daff
thecurioushackr <dawaunw@thecurioushacker.com>
2025-01-22 10:07:57 -0600

    initial

--- src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export interface ProductivityInsights {
-  requiresIntervention: boolean;
-  currentActivity: {
-    name: string;
-    isUnproductive: boolean;
-  };
-  timeSpent: string;
-  recommendedActivity: string;
-} 
